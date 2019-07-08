3a6771765eb6f9c3c7f98084ddc48b1e0cc099e5
s33dco <code@s33d.co>
2019-07-08 14:41:00 +0100

    lifecycle componentDidMount with axios http request to set state, passed down as props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Fragment, Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import axios from 'axios'
 import './App.css'
 
 class App extends Component {
+	state = {
+		users: [],
+		loading: false
+	}
+
+	async componentDidMount() {
+		this.setState({ loading: true })
+		const res = await axios.get('https://api.github.com/users')
+		this.setState({ users: res.data, loading: false })
+	}
+
 	render() {
 		return (
 			<Fragment>
 				{/* default props set in Navbar component can overwrite below */}
 				<Navbar />
 				<div className='container'>
-					<Users />
+					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</Fragment>
 		)
