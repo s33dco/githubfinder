a5b5a74f6c5beb623b6bd5bc88579e06e63116f4
s33dco <code@s33d.co>
2019-07-09 00:36:52 +0100

    Repos and RepoItem components and data, basic app complete, pre refactor to Hooks

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
 	state = {
 		users: [],
 		user: {},
+		repos: [],
 		loading: false,
 		alert: null
 	}
@@ -38,6 +39,17 @@ class App extends Component {
 		this.setState({ user: res.data, loading: false })
 	}
 
+	getUserRepos = async username => {
+		this.setState({ loading: true })
+
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+				process.env.REACT_APP_GITHUB_CLIENT_ID
+			}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		)
+		this.setState({ repos: res.data, loading: false })
+	}
+
 	clearUsers = () => {
 		this.setState({ users: [], loading: false })
 	}
@@ -48,7 +60,7 @@ class App extends Component {
 	}
 
 	render() {
-		const { users, loading, user } = this.state
+		const { users, loading, user, repos } = this.state
 		return (
 			<Router>
 				<Fragment>
@@ -77,7 +89,14 @@ class App extends Component {
 								exact
 								path='/user/:login'
 								render={props => (
-									<User {...props} getUser={this.getUser} user={user} loading={loading} />
+									<User
+										{...props}
+										getUser={this.getUser}
+										getUserRepos={this.getUserRepos}
+										user={user}
+										repos={repos}
+										loading={loading}
+									/>
 								)}
 							/>
 						</Switch>
