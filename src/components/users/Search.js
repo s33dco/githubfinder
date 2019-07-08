00a8f4a894bb34fe2ee416cb1513d35c7cb70cfc
s33dco <code@s33d.co>
2019-07-08 16:27:12 +0100

    clear users from state

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 export class Search extends Component {
@@ -7,7 +7,9 @@ export class Search extends Component {
 	}
 
 	static propTypes = {
-		searchUsers: PropTypes.func.isRequired
+		searchUsers: PropTypes.func.isRequired,
+		clearUsers: PropTypes.func.isRequired,
+		showClear: PropTypes.bool.isRequired
 	}
 
 	onSubmit = e => {
@@ -15,22 +17,35 @@ export class Search extends Component {
 		this.props.searchUsers(this.state.text) // pass up to App as arguement to function
 		this.setState({ text: '' }) // reset from field
 	}
+	clear = e => {
+		this.props.clear()
+		this.setState({ text: '' }) // reset from field
+	}
 
 	onChange = e => this.setState({ [e.target.name]: e.target.value }) // use bracket notation to get 'text' handy for multi use
 
 	render() {
+		const { showClear, clearUsers } = this.props
+
 		return (
-			<form onSubmit={this.onSubmit} className='form'>
-				{/* controlled input with onChange event, component level state */}
-				<input
-					type='text'
-					name='text'
-					placeholder='Search Users...'
-					value={this.state.text}
-					onChange={this.onChange}
-				/>
-				<input type='submit' value='Search' className='btn btn-dark btn-block' />
-			</form>
+			<Fragment>
+				<form onSubmit={this.onSubmit} className='form'>
+					{/* controlled input with onChange event, component level state */}
+					<input
+						type='text'
+						name='text'
+						placeholder='Search Users...'
+						value={this.state.text}
+						onChange={this.onChange}
+					/>
+					<input type='submit' value='Search' className='btn btn-dark btn-block' />
+				</form>
+				{showClear && (
+					<button className='btn btn-light btn-block' onClick={clearUsers}>
+						Clear
+					</button>
+				)}
+			</Fragment>
 		)
 	}
 }
