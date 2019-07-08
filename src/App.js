e84b399069e0397e603848b346a56e755c24c226
s33dco <code@s33d.co>
2019-07-08 11:12:05 +0100

    jsx conditionals and expressions recap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
 import './App.css'
 
 class App extends Component {
+	// method as part of class
+	baz = () => 'Baz'
+
 	render() {
+		const name = 'A Variable'
+		const foo = () => 'Bar'
+		const loading = false
+		const showName = false
+
 		return (
-			<div className='App'>
-				<h1>Hello from React</h1>
-			</div>
+			<Fragment>
+				{loading ? (
+					<h4>Loading...</h4>
+				) : (
+					<div>
+						<h1>Hello {showName && name}</h1>
+						<h2>Use a fragment instead of parent div of jsx</h2>
+						<h3>from a function {foo()}</h3>
+						<h3>from a class method {this.baz()}</h3>
+					</div>
+				)}
+			</Fragment>
 		)
 	}
 }
