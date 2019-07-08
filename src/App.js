46da6638fb4056895ebe7d57b99f724d1d83cf3f
s33dco <code@s33d.co>
2019-07-08 17:17:16 +0100

    add reat router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Fragment, Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
+import About from './components/pages/About'
 import axios from 'axios'
 import './App.css'
 
@@ -47,20 +49,33 @@ class App extends Component {
 	render() {
 		const { users, loading } = this.state
 		return (
-			<Fragment>
-				{/* default props set in Navbar component can overwrite below */}
-				<Navbar />
-				<div className='container'>
-					<Alert alert={this.state.alert} />
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						setAlert={this.setAlert}
-						showClear={users.length > 0 ? true : false}
-					/>
-					<Users loading={loading} users={users} />
-				</div>
-			</Fragment>
+			<Router>
+				<Fragment>
+					{/* default props set in Navbar component can overwrite below */}
+					<Navbar />
+					<div className='container'>
+						<Alert alert={this.state.alert} />
+						<Switch>
+							<Route
+								exact
+								path='/'
+								render={props => (
+									<Fragment>
+										<Search
+											searchUsers={this.searchUsers}
+											clearUsers={this.clearUsers}
+											setAlert={this.setAlert}
+											showClear={users.length > 0 ? true : false}
+										/>
+										<Users loading={loading} users={users} />
+									</Fragment>
+								)}
+							/>
+							<Route exact path='/about' component={About} />
+						</Switch>
+					</div>
+				</Fragment>
+			</Router>
 		)
 	}
 }
