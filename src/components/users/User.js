2fd9dec78c5bd7bf06bac67b332c80707843cfae
s33dco <code@s33d.co>
2019-07-10 11:36:26 +0100

    Convert User component from Class to functional with useState hook

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,106 +1,109 @@
-import React, { Fragment, Component } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import Spinner from '../layout/Spinner'
 import PropTypes from 'prop-types'
 import Repos from '../repos/Repos'
 import { Link } from 'react-router-dom'
 
-export class User extends Component {
-	componentDidMount() {
-		this.props.getUser(this.props.match.params.login) // grab param from url :login
-		this.props.getUserRepos(this.props.match.params.login) // grab array of repos
-	}
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+	// useEffect hook to replace componentDidMount()
 
-	static propTypes = {
-		loading: PropTypes.bool,
-		user: PropTypes.object.isRequired,
-		getUser: PropTypes.func.isRequired,
-		getUserRepos: PropTypes.func.isRequired,
-		repos: PropTypes.array.isRequired
-	}
+	useEffect(() => {
+		getUser(match.params.login) // grab param from url :login
+		getUserRepos(match.params.login) // grab array of repos
+		// eslint has populated the brackets below
+	}, [getUser, getUserRepos, match.params.login]) // need second arguement to stop request loop, useEffect runs on anyupdate, the calls to getUser / getUserRepos are constantly updating the loop, ie it keeps making the requests. 2nd arguement [] allows conditions to beset for updating, empty brackets for just once. No conditions to reupdate.
 
-	render() {
-		// destructure this.props.user
-		const {
-			name,
-			company,
-			avatar_url,
-			location,
-			bio,
-			blog,
-			login,
-			html_url,
-			followers,
-			following,
-			public_repos,
-			public_gists,
-			hireable
-		} = this.props.user
+	// componentDidMount() {
+	// 	this.props.getUser(match.params.login) // grab param from url :login
+	// 	this.props.getUserRepos(match.params.login) // grab array of repos
+	// }
+	// destructure props.user
+	const {
+		name,
+		company,
+		avatar_url,
+		location,
+		bio,
+		blog,
+		login,
+		html_url,
+		followers,
+		following,
+		public_repos,
+		public_gists,
+		hireable
+	} = user
 
-		const { loading, repos } = this.props
-
-		if (loading) return <Spinner />
-		return (
-			<Fragment>
-				<Link to='/' className='btn btn-light'>
-					Back to search
-				</Link>
-				Hireable :{' '}
-				{hireable ? (
-					<i className='fas fa-check text-success' />
-				) : (
-					<i className='fas fa-times-circle text-danger' />
-				)}
-				<div className='card grid-2'>
-					<div className='all-center'>
-						<img src={avatar_url} className='round-img' alt='' style={{ width: '150px' }} />
-						<h1>{name}</h1>
-						<p>{location}</p>
-					</div>
-					<div>
-						{bio && (
-							<Fragment>
-								<h3>Bio</h3>
-								<p>{bio}</p>
-							</Fragment>
-						)}
-						<a href={html_url} className='btn btn-dark my-1'>
-							Visit Github Profile
-						</a>
-						<ul>
-							<li>
-								{login && (
-									<Fragment>
-										<strong>Username: </strong> {login}
-									</Fragment>
-								)}
-							</li>
-							<li>
-								{company && (
-									<Fragment>
-										<strong>Company: </strong> {company}
-									</Fragment>
-								)}
-							</li>
-							<li>
-								{blog && (
-									<Fragment>
-										<strong>Website: </strong> {blog}
-									</Fragment>
-								)}
-							</li>
-						</ul>
-					</div>
+	if (loading) return <Spinner />
+	return (
+		<Fragment>
+			<Link to='/' className='btn btn-light'>
+				Back to search
+			</Link>
+			Hireable :{' '}
+			{hireable ? (
+				<i className='fas fa-check text-success' />
+			) : (
+				<i className='fas fa-times-circle text-danger' />
+			)}
+			<div className='card grid-2'>
+				<div className='all-center'>
+					<img src={avatar_url} className='round-img' alt='' style={{ width: '150px' }} />
+					<h1>{name}</h1>
+					<p>{location}</p>
 				</div>
-				<div className='card text-center'>
-					<div className='badge badge-primary'>Followers: {followers}</div>
-					<div className='badge badge-success'>Following: {following}</div>
-					<div className='badge badge-light'>Public Repos: {public_repos}</div>
-					<div className='badge badge-dark'>Public Gists: {public_gists}</div>
+				<div>
+					{bio && (
+						<Fragment>
+							<h3>Bio</h3>
+							<p>{bio}</p>
+						</Fragment>
+					)}
+					<a href={html_url} className='btn btn-dark my-1'>
+						Visit Github Profile
+					</a>
+					<ul>
+						<li>
+							{login && (
+								<Fragment>
+									<strong>Username: </strong> {login}
+								</Fragment>
+							)}
+						</li>
+						<li>
+							{company && (
+								<Fragment>
+									<strong>Company: </strong> {company}
+								</Fragment>
+							)}
+						</li>
+						<li>
+							{blog && (
+								<Fragment>
+									<strong>Website: </strong> {blog}
+								</Fragment>
+							)}
+						</li>
+					</ul>
 				</div>
-				<Repos repos={repos} />
-			</Fragment>
-		)
-	}
+			</div>
+			<div className='card text-center'>
+				<div className='badge badge-primary'>Followers: {followers}</div>
+				<div className='badge badge-success'>Following: {following}</div>
+				<div className='badge badge-light'>Public Repos: {public_repos}</div>
+				<div className='badge badge-dark'>Public Gists: {public_gists}</div>
+			</div>
+			<Repos repos={repos} />
+		</Fragment>
+	)
+}
+
+User.propTypes = {
+	loading: PropTypes.bool,
+	user: PropTypes.object.isRequired,
+	getUser: PropTypes.func.isRequired,
+	getUserRepos: PropTypes.func.isRequired,
+	repos: PropTypes.array.isRequired
 }
 
 export default User
